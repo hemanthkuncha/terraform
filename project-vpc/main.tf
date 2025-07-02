ff19737314d5f2a49646ec00ad0ad3aa81d1e14c
hemanthkuncha <168633321+hemanthkuncha@users.noreply.github.com>
2025-07-02 12:22:25 +0530

    Update main.tf

--- project-vpc/main.tf
+++ project-vpc/main.tf
@@ -3,15 +3,9 @@ provider "aws" {
 }
 
 module "vpc" {
-<<<<<<< HEAD
   source             = "./modules/vpc"
-  vpc_cidr           = "10.10.0.0/16"
-  public_subnet_cidrs = ["10.10.1.0/24", "10.10.2.0/24"]
-=======
-  source               = "./modules/vpc"
   vpc_cidr             = "10.10.0.0/16"
   public_subnet_cidrs  = ["10.10.1.0/24", "10.10.2.0/24"]
->>>>>>> 2e310a8 (Initial commit)
   enable_dns_hostnames = true
 }
 
